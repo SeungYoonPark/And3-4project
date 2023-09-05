884805a3ff97bc50d1738430feaa9527f374e694
yeonghyeon <agilecatch@gmail.com>
2023-09-05 18:50:14 +0900

    [FEAT] : ⚙️ 어댑터, 싱글톤데이터 연결
    - 어댑터와 싱글톤 데이터를 연결하여 리사이클러뷰에 보이도록 구현
    - 리사이클러뷰 아이템을 클릭하여 디테일 페이지로 화면이 전환되도록 구현하였습니다.

--- app/src/main/java/com/example/and3_4project/Contact/ContactList.kt
+++ app/src/main/java/com/example/and3_4project/Contact/ContactList.kt
@@ -3,7 +3,6 @@ package com.example.and3_4project.Contact
 data class ContactList(
     var profileImg: Int,
     val contactName: String,
-    var heartImg :Int,
     val phoneNumber:String,
     val email:String,
     var notification:String,
--- app/src/main/java/com/example/and3_4project/DetailActivty.kt
+++
@@ -1,4 +0,0 @@
-package com.example.and3_4project
-
-class DetailActivty {
-}
