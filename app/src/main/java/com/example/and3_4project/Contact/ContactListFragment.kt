2b19f0113c2a7e9dfae3dcf7ea6ce4fcab55b81f
seunghoon Lee <hara9052@naver.com>
2023-09-06 14:22:23 +0900

    [FEAT] : ⚙️ contactListFragment 메뉴 누르면 변경

--- app/src/main/java/com/example/and3_4project/Contact/ContactListFragment.kt
+++ app/src/main/java/com/example/and3_4project/Contact/ContactListFragment.kt
@@ -1,45 +1,71 @@
 package com.example.and3_4project.Contact
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.and3_4project.Main.InfoSingleton
+import com.example.and3_4project.R
 import com.example.and3_4project.databinding.FragmentContactListBinding
 
 
 class ContactListFragment : Fragment() {
 
     private lateinit var binding: FragmentContactListBinding
+    private var viewType = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
 
     // 처음에 그려질때 호출되는 콜백 메서드
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
 
         binding = FragmentContactListBinding.inflate(inflater, container, false)
-
+        setHasOptionsMenu(true)
         //싱글톤 연결하기
-        val contactList=InfoSingleton.getcontactList()
+        val contactList = InfoSingleton.getcontactList()
         val adapter = RecyclerViewAdapter(contactList)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(activity)
-
-
-
         adapter.itemClick = object : RecyclerViewAdapter.ItemClick {
             override fun onClick(view: View, position: Int) {
-                startActivity(ContactDetailActivity.newIntentForDetail(context,position))
+                startActivity(ContactDetailActivity.newIntentForDetail(context, position))
             }
         }
 
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.grid_menu -> {
+                    if (viewType){
+                    it.setIcon(R.drawable.list)}
+                    else{
+                        it.setIcon(R.drawable.grid)
+                    }
+                    viewType = !viewType
+                    true
+                }
+                else -> {
+                }
+            }
+            true
+        }
         return binding.root
+
+
     }
 
 
@@ -49,6 +75,6 @@ class ContactListFragment : Fragment() {
         frag.arguments = args
         return frag
     }
-
-
 }
+
+
