e2485f91bdea85aaec86973a146368869adcee56
yeonghyeon <agilecatch@gmail.com>
2023-09-07 16:08:46 +0900

    [DESIGN] : 🎨 다이얼로그
    - 토글버튼 디자인 변경
    - 주소록에서 불러오기 아이콘 변경

--- app/src/main/java/com/example/and3_4project/Main/InfoSingleton.kt
+++ app/src/main/java/com/example/and3_4project/Main/InfoSingleton.kt
@@ -8,68 +8,68 @@ object InfoSingleton {
     var contactList: MutableList<ContactList> = mutableListOf(
         ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름1",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.dog1),
+            "김영현",
+            "010-6688-3596",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름2",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.cat),
+            "나유성",
             "010-1234-1234",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름3",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.cat2),
+            "박승윤",
+            "010-1111-2222",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름4",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.cat10),
+            "이승훈",
+            "010-3333-4444",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름5",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.cat14),
+            "김뽀삐",
+            "010-0987-7789",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름6",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.bbang),
+            "빵빵이",
+            "010-0000-0000",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름7",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.oksun),
+            "옥지얌",
+            "010-5757-2734",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름8",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.poketmon),
+            "이상해씨",
+            "010-2424-9865",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         ), ContactList(
 
 
-            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.profile_white),
-            "이름9",
-            "010-1234-1234",
+            Uri.parse("android.resource://" + R::class.java.`package`?.name + "/" + R.drawable.poketmon2),
+            "피카츄",
+            "010-9999-7654",
             "kyhi525@naver.com",
-            "dhgn 5시에 알림이 있습니다."
+            "알림이 없습니다."
         )
     )
 
